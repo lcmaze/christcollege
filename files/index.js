585b031a6d473c585f3ccd49fddc9f0904f0042f
lcmaze <lcmazeweb@gmail.com>
2020-03-20 19:13:26 +0530

    update

---
+++ files/index.js
@@ -0,0 +1,29 @@
+$('.carousal-one').owlCarousel({
+    loop:true,
+    margin:0,
+    nav: false,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        1200:{
+            items:3
+        }
+    }
+})
+
+$('.carousal-two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav: false,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
